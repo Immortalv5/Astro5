df5c2e902eff9863c1c0b4675d89203e79ddedcb
Immortalv5 <saitejaaoe@gmail.com>
2021-01-20 14:42:57 +0000

    deleting calls

--- Astro5/urls.py
+++ Astro5/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url,include
 from Astro import views
-import django_twilio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
