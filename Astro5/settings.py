3a4e0f7a8fde1706c1607e81867c350302a4f745
Immortalv5 <saitejaaoe@gmail.com>
2021-01-30 14:53:05 +0000

    staticfiles

--- Astro5/settings.py
+++ Astro5/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Astro',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
