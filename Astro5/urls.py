3a1f31562a5ddad3faff9fdc2baa3067f28926f5
Immortalv5 <saitejaaoe@gmail.com>
2021-02-15 13:54:12 +0000

    password error

--- Astro5/urls.py
+++ Astro5/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     url(r'^callback/$', views.callback, name = 'callback'),
     url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    url(r'^password_reset_done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
